a0599673841611e83e5023e10070f321783a3401
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-02-07 19:08:57 -0300

    Format files & add things

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,12 @@ module.exports = {
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    "plugin:prettier/recommended",
   ],
   env: {
     "cypress/globals": true,
   },
-  plugins: ["cypress"],
+  plugins: ["cypress", "prettier"],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
@@ -21,20 +21,23 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": ["error"],
     curly: ["error"],
-    "no-else-return": ["error", {
-      allowElseIf : false
-    }],
+    "no-else-return": [
+      "error",
+      {
+        allowElseIf: false,
+      },
+    ],
     "no-console": ["warn"],
     "prefer-destructuring": [
       "error",
       {
         object: true,
-        array: false
-      }
+        array: false,
+      },
     ],
     "react/destructuring-assignment": ["error", "always"],
     "react/react-in-jsx-scope": ["off"],
     "react/display-name": ["off"],
-    "@typescript-eslint/no-empty-function": ["off"]
-  }
+    "@typescript-eslint/no-empty-function": ["off"],
+  },
 };
--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,62 +1,24 @@
-import type { Password, User } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import type { User } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import { prisma } from '~/db.server'
 
-import { prisma } from "~/db.server";
-
-export type { User } from "@prisma/client";
-
-export async function getUserById(id: User["id"]) {
-  return prisma.user.findUnique({ where: { id } });
-}
-
-export async function getUserByEmail(email: User["email"]) {
-  return prisma.user.findUnique({ where: { email } });
-}
-
-export async function createUser(email: User["email"], password: string) {
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  return prisma.user.create({
-    data: {
+export const getUserByEmail = (email: string) =>
+  prisma.user.findUnique({
+    where: {
       email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
     },
-  });
+  })
+
+export const createUserByEmailAndPassword = (user: User) => {
+  user.password = bcrypt.hashSync(user.password, 12)
+  return prisma.user.create({
+    data: user,
+  })
 }
 
-export async function deleteUserByEmail(email: User["email"]) {
-  return prisma.user.delete({ where: { email } });
-}
-
-export async function verifyLogin(
-  email: User["email"],
-  password: Password["hash"]
-) {
-  const userWithPassword = await prisma.user.findUnique({
-    where: { email },
-    include: {
-      password: true,
+export const getUserById = (id: string) =>
+  prisma.user.findUnique({
+    where: {
+      id,
     },
-  });
-
-  if (!userWithPassword || !userWithPassword.password) {
-    return null;
-  }
-
-  const isValid = await bcrypt.compare(
-    password,
-    userWithPassword.password.hash
-  );
-
-  if (!isValid) {
-    return null;
-  }
-
-  const { password: _password, ...userWithoutPassword } = userWithPassword;
-
-  return userWithoutPassword;
-}
+  })
