8bff00a03ad811ba3311c7d22c55a79428e985a6
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-02-28 12:28:08 -0300

    Fix prettier and linter

--- app/utils/jwt.ts
+++ app/utils/jwt.ts
@@ -1,29 +1,29 @@
-import jwt from "jsonwebtoken";
-import type { User } from "@prisma/client";
-import type { Secret } from "jsonwebtoken";
-import { createHash } from "crypto";
+import jwt from 'jsonwebtoken'
+import type { User } from '@prisma/client'
+import type { Secret } from 'jsonwebtoken'
+import { createHash } from 'crypto'
 
-const accessSecret = process.env.JWT_ACCESS_SECRET as Secret;
-const refreshSecret = process.env.JWT_REFRESH_SECRET as Secret;
+const accessSecret = process.env.JWT_ACCESS_SECRET as Secret
+const refreshSecret = process.env.JWT_REFRESH_SECRET as Secret
 
 export const generateAccessToken = (user: User) =>
   jwt.sign({ userId: user.id }, accessSecret, {
-    expiresIn: "5m",
-  });
+    expiresIn: '5m',
+  })
 
 export const generateRefreshToken = (user: User, jti: string) =>
-  jwt.sign({ userId: user.id, jti }, refreshSecret, { expiresIn: "8h" });
+  jwt.sign({ userId: user.id, jti }, refreshSecret, { expiresIn: '8h' })
 
 export const generateTokens = (user: User, jti: string) => {
-  const accessToken = generateAccessToken(user);
-  const refreshToken = generateRefreshToken(user, jti);
+  const accessToken = generateAccessToken(user)
+  const refreshToken = generateRefreshToken(user, jti)
 
   return {
     accessToken,
     refreshToken,
-  };
-};
+  }
+}
 
 export const hashToken = (token: string) => {
-  return createHash("sha512").update(token).digest("hex");
-};
+  return createHash('sha512').update(token).digest('hex')
+}
