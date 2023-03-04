ba30503092db0da171b003a9e5305bc942938fd0
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-03-04 11:19:29 -0300

    Add a little bit of responsiveness to dialog

--- app/providers/dialog.tsx
+++ app/providers/dialog.tsx
@@ -23,7 +23,7 @@ export const DialogProvider: FC<PropsWithChildren> = ({ children }) => {
         <div className="fixed inset-0 z-10">
           <div className="absolute inset-0 bg-black opacity-20"></div>
           <div className="relative flex h-screen">
-            <div className="min-h-1/2 max-h-3/4 m-auto w-1/2 rounded-2xl bg-foreground">
+            <div className="md:min-h-1/2 md:max-h-3/4 h-full w-full bg-foreground md:m-auto md:w-1/2 md:rounded-2xl">
               {content}
             </div>
           </div>
