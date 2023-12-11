d6187e44e2bc6afff7df50dbcaa111e7ca730b02
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-12-11 10:18:01 -0300

    Use env for firebase admin sdk

--- app/infra/firebase.server.ts
+++ app/infra/firebase.server.ts
@@ -2,10 +2,13 @@ import type { App, ServiceAccount } from 'firebase-admin/app'
 import type { Auth } from 'firebase-admin/auth'
 import { initializeApp, getApps, cert, getApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
-import sdk from '../../expensable-firebase-adminsdk.json'
+import invariant from 'tiny-invariant'
 
 let app: App
 let auth: Auth
+
+invariant(process.env.FIREBASE_ADMINSDK, 'FIREBASE_ADMINSDK must be set')
+const sdk = JSON.parse(process.env.FIREBASE_ADMINSDK as string)
 
 if (getApps().length === 0) {
   app = initializeApp({
