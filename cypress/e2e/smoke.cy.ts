4c300ec67e27382b9aa90beb98336db4f49cee9e
Murilo Wolfart <murilo.wolfart@mentorspaces.com>
2024-03-31 14:31:18 -0300

    Update smoke test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import * as i18n from '../../public/locales/en/common.json'
 
 describe('smoke tests', () => {
   afterEach(() => {
@@ -12,46 +13,67 @@ describe('smoke tests', () => {
       password: faker.internet.password(),
     }
 
+    cy.intercept('POST', '/create-user', (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          success: true,
+          error: null,
+        },
+      })
+    }).as('createUser')
+
+    // TODO catch login?
+
     cy.visit('/')
     cy.location('pathname').should('eq', '/login')
 
-    // cy.findByRole('button', { name: i18n.en.auth['create-account'] }).click()
+    cy.findByRole('button', { name: i18n.auth['create-account'] }).click()
     cy.location('pathname').should('eq', '/create-user')
     cy.get('input[name="email"]').type(account.email)
     cy.get('input[name="name"]').type(account.name)
     cy.get('input[name="password"]').type(account.password)
     cy.get('button[type="submit"]').click()
-    // TODO continue
 
-    // cy.get('input[type="email"]').type(loginForm.email)
-    // cy.get('input[type="password"]').type(loginForm.password)
-    cy.get('button[type="submit"]').click()
+    cy.wait('@createUser').then((interception) => {
+      expect(interception.response?.statusCode).to.equal(200)
+      expect(interception.response?.body?.success).to.equal(true)
 
-    cy.location('pathname').should('eq', '/')
-    // cy.findByRole('button', { name: i18n.en.common.logout }).click()
-    cy.location('pathname').should('eq', '/login')
+      cy.findByRole('button', {
+        name: i18n.auth['already-have-account'],
+      }).click()
+      cy.location('pathname').should('eq', '/login')
+
+      cy.get('input[type="email"]').type(account.email)
+      cy.get('input[type="password"]').type(account.password)
+      cy.get('button[type="submit"]').click()
+
+      cy.location('pathname').should('eq', '/')
+      cy.findByRole('button', { name: i18n.common.logout }).click()
+      cy.location('pathname').should('eq', '/login')
+    })
   })
 
-  it('should allow you to make a note', () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    }
-    cy.login()
+  // it('should allow you to make a note', () => {
+  //   const testNote = {
+  //     title: faker.lorem.words(1),
+  //     body: faker.lorem.sentences(1),
+  //   }
+  //   cy.login()
 
-    cy.visitAndCheck('/')
+  //   cy.visitAndCheck('/')
 
-    cy.findByRole('link', { name: /notes/i }).click()
-    cy.findByText('No notes yet')
+  //   cy.findByRole('link', { name: /notes/i }).click()
+  //   cy.findByText('No notes yet')
 
-    cy.findByRole('link', { name: /\+ new note/i }).click()
+  //   cy.findByRole('link', { name: /\+ new note/i }).click()
 
-    cy.findByRole('textbox', { name: /title/i }).type(testNote.title)
-    cy.findByRole('textbox', { name: /body/i }).type(testNote.body)
-    cy.findByRole('button', { name: /save/i }).click()
+  //   cy.findByRole('textbox', { name: /title/i }).type(testNote.title)
+  //   cy.findByRole('textbox', { name: /body/i }).type(testNote.body)
+  //   cy.findByRole('button', { name: /save/i }).click()
 
-    cy.findByRole('button', { name: /delete/i }).click()
+  //   cy.findByRole('button', { name: /delete/i }).click()
 
-    cy.findByText('No notes yet')
-  })
+  //   cy.findByText('No notes yet')
+  // })
 })
